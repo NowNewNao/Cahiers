5fb336ce7d86e8a3a53f9be538635f3d25a2ce59
nao <naoyosetani@gmail.com>
2022-06-27 20:57:00 +0800

    Incremental: Use Storyblok in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+storyblokInit({
+  accessToken: "your-preview-token",
+  use: [apiPlugin]
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
